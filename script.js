7080a8cbd4b2d6a6ddf361c73a1250bcfd1532ac
INT019 <fernandothushini8@gmail.com>
2024-02-29 23:37:56 +0530

    Commit
    JavaScript

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll( '[data-number]' )
+const operationButtons = document.querySelectorAll( '[data-operation]' )
+const equalsButton = document.querySelector( '[data-equals]' )
+const deleteButton = document.querySelector( '[data-delete]' )
+const allClearButton = document.querySelector( '[data-all-clear]' )
+const previewOperandTextElement = document.querySelector( '[data-preview-operand]' )
+const currentOperandTextElement = document.querySelector( '[data-current-operand]' )
