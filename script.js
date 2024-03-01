f2a369ad39fd3d2c66e8e0384310232a07208089
INT019 <fernandothushini8@gmail.com>
2024-03-01 21:57:02 +0530

    Commit
    Calculator class

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+class Calculator
+{
+    constructor ( previewOperandTextElement, currentOperandTextElement )
+    {
+        this.previewOperandTextElement = previewOperandTextElement;
+        this.currentOperandTextElement = currentOperandTextElement;
+        this.clear()
+    }
+
+    clear ()
+    {
+        this.currentOperandTextElement = ''
+        this.previewOperandTextElement = ''
+        this.operation = undefined
+    }
+
+    delete ()
+    {
+
+    }
+
+    appendNumber ( number )
+    {
+
+    }
+
+    chooseOperation ( operation )
+    {
+
+    }
+
+    compute ()
+    {
+
+    }
+
+    updateDisplay ()
+    {
+
+    }
+}
+
 const numberButtons = document.querySelectorAll( '[data-number]' )
 const operationButtons = document.querySelectorAll( '[data-operation]' )
 const equalsButton = document.querySelector( '[data-equals]' )
