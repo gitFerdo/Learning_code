18004a24fbc65e34f2d32377ef515987218be6ad
INT019 <fernandothushini8@gmail.com>
2024-03-05 23:03:49 +0530

    Finished

--- script.js
+++ script.js
@@ -16,5 +16,22 @@ function startVideo ()
 
 video.addEventListener( 'playing', () =>
 {
-    console.log( 'playing' )
+    const canvas = faceapi.createCanvasFromMedia( video )
+    document.body.append( canvas )
+    const displaySize = { width: video.width, height: video.height }
+    faceapi.matchDimensions( canvas, displaySize )
+
+    setInterval( async () =>
+    {
+        const detections = await faceapi.detectAllFaces( video, new faceapi.TinyFaceDetectorOptions() ).withFaceLandmarks().withFaceExpressions()
+
+        console.log( detections )
+
+        const resizeDetections = faceapi.resizeResults( detections, displaySize )
+        canvas.getContext( '2d' ).clearRect( 0, 0, canvas.width, canvas.height )
+
+        faceapi.draw.drawDetections( canvas, resizeDetections )
+        faceapi.draw.drawFaceLandmarks( canvas, resizeDetections )
+        faceapi.draw.drawFaceExpressions( canvas, resizeDetections )
+    }, 100 )
 } );
