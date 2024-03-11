e4599c90836e6862b945478862a64b2d146b3acf
INT019 <fernandothushini8@gmail.com>
2024-03-11 23:32:30 +0530

    Final Commit

--- passport-config.js
+++ passport-config.js
@@ -30,7 +30,11 @@ function initialize ( passport, getUserByEmail )
     passport.use( new LocalStrategy( { usernameField: 'email' }, authenticateUser ) )
 
     passport.serializeUser( ( user, done ) => { } )
-    passport.deserializeUser( ( id, done ) => { } )
+    passport.serializeUser( ( user, done ) => done( null, user.id ) )
+    passport.deserializeUser( ( id, done ) =>
+    {
+        return done( null, getUserById( id ) )
+    } )
 }
 
 module.exports = initialize
--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const initializePassport = require( './passport-config' );
 const passport = require( 'passport' );
 const flash = require( 'express-flash' );
 const session = require( 'express-session' );
+const methodOverride = require( 'method-override' );
 
 initializePassport(
     passport,
-    email => users.find( user => user.email === email )
+    email => users.find( user => user.email === email ),
+    id => users.find( user => user.id === id )
 )
 
 const users = []
@@ -28,47 +30,94 @@ app.use( session( {
 } ) )
 app.use( passport.initialize() )
 app.use( passport.session() )
+app.use( methodOverride( '_method' ) )
 
-app.get( '/', ( req, res ) =>
-{
-    res.render( "index.ejs", { name: 'Kamal' } );
-} )
-
-app.get( '/login', ( req, res ) =>
-{
-    res.render( "login.ejs" );
-} )
-
-app.post( '/login', passport.authenticate( 'local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-} ) )
-
-app.get( '/register', ( req, res ) =>
-{
-    res.render( "register.ejs" );
-} )
-
-app.post( '/register', async ( req, res ) =>
-{
-    try
+app.get(
+    '/',
+    checkAuthenticated,
+    ( req, res ) =>
     {
-        const hashedPassword = await bcrypt.hash( req.body.password, 10 )
+        res.render( "index.ejs", { name: req.user.name } );
+    } )
 
-        users.push( {
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
+app.get(
+    '/login',
+    checkNotAuthenticated,
+    ( req, res ) =>
+    {
+        res.render( "login.ejs" );
+    } )
+
+app.post(
+    '/login',
+    checkNotAuthenticated,
+    passport.authenticate( 'local', {
+        successRedirect: '/',
+        failureRedirect: '/login',
+        failureFlash: true
+    } ) )
+
+app.get(
+    '/register',
+    checkNotAuthenticated,
+    ( req, res ) =>
+    {
+        res.render( "register.ejs" );
+    } )
+
+app.post(
+    '/register',
+    checkNotAuthenticated,
+    async ( req, res ) =>
+    {
+        try
+        {
+            const hashedPassword = await bcrypt.hash( req.body.password, 10 )
+
+            users.push( {
+                id: Date.now().toString(),
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            } )
+
+            res.redirect( '/login' )
+        } catch {
+            res.redirect( '/register' )
+        }
+
+        console.log( users )
+    } )
+
+app.delete(
+    '/logout',
+    ( req, res ) =>
+    {
+        req.logOut( () =>
+        {
+            res.redirect( '/login' )
         } )
+    }
+)
 
-        res.redirect( '/login' )
-    } catch {
-        res.redirect( '/register' )
+function checkAuthenticated ( req, res, next )
+{
+    if ( req.isAuthenticated() )
+    {
+        return next()
     }
 
-    console.log( users )
-} )
+    res.redirect( '/login' )
+}
+
+function checkNotAuthenticated ( req, res, next )
+{
+    if ( req.isAuthenticated() )
+    {
+        return res.redirect( '/' )
+    }
+
+    next()
+}
 
 app.listen( 3000 );
