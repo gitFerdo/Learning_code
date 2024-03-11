194dbc7a35e14956b7a00f190d76db88fea1f477
INT019 <fernandothushini8@gmail.com>
2024-03-11 23:33:08 +0530

    Final Commit

--- passport-config.js
+++ passport-config.js
@@ -1,7 +1,7 @@
 const LocalStrategy = require( 'passport-local' ).Strategy
 const bcrypt = require( 'bcrypt' )
 
-function initialize ( passport, getUserByEmail )
+function initialize ( passport, getUserByEmail, getUserById )
 {
     const authenticateUser = async ( email, password, done ) =>
     {
@@ -29,7 +29,6 @@ function initialize ( passport, getUserByEmail )
 
     passport.use( new LocalStrategy( { usernameField: 'email' }, authenticateUser ) )
 
-    passport.serializeUser( ( user, done ) => { } )
     passport.serializeUser( ( user, done ) => done( null, user.id ) )
     passport.deserializeUser( ( id, done ) =>
     {
