a22c99fa0974f9145a718b018afe212d9c858ceb
amasha <fernandothushini8@gmail.com>
2024-07-23 14:36:11 +0530

    connect to database

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+// Connect to the database
+mongoose.connect(process.env.DATABASE_URL);
+
+const db = mongoose.connection;
+
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to database"));
 
 app.listen(3000, () => console.log("Server listening on port 3000"));
